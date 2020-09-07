00b42f8904c9e18799f9bb072912c101d11fb9ec
chandra655 <chandra.penugonda@unacademy.com>
2020-09-07 21:28:40 +0530

    work on callApiMiddleware wip

--- redux/middlewares/callApiMiddleware.js
+++ redux/middlewares/callApiMiddleware.js
@@ -1,5 +1,33 @@
-export default function callApiMiddleware() {
+import fetch from 'fetch';
+import { API } from '../actions/types';
+import { apiStart } from '../actions/api';
+
+function checkStatus(res) {
+    if (res.ok) return res;
+    const error = new Error(res.statusText);
+    error.response = res;
+    return Promise.reject(error);
+}
+
+export default function callApiMiddleware({ dispatch }) {
     return (next) => (action) => {
-        return next(action);
+        if (action.type !== API) return next(action);
+        const { url, method, data, onSuccess, onFailure, label } = action.payload;
+        const dataOrParams = ['GET', 'DELETE'].includes(method) ? 'params' : 'data';
+        if (label) {
+            dispatch(apiStart(`${label}_REQUEST`));
+        }
+        const headers = {
+            authorization: `Bearer `,
+            'Content-Type': 'application/json'
+        };
+        return fetch(url, {
+            method,
+            body: JSON.stringify(data),
+            headers,
+            credentials: 'same-origin'
+        })
+            .then(checkStatus)
+            .then();
     };
 }
